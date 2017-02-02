4be5c0c7efafcdb213b48f37b7a3ee9036511238
Denis Mordvinov <denis.mordvinov@rosberry.com>
2017-02-02 14:51:29 +0600

    password page for ios

--- features/ios/pages/password_page.rb
+++ features/ios/pages/password_page.rb
@@ -7,9 +7,92 @@ class PasswordPage < Calabash::IBase
 
   def trait
 
-    "* id:'join_input_et'"   #this is a forgotPassword button
+    "button marked:'Forgot password?'"   #this is a forgotPassword button
 
   end
+
+
+  def backButton
+
+   "button marked:'iconTopBack'"
+
+  end
+
+
+  def textField
+
+    "UITextField"
+  end
+
+  def eyeButton
+
+   "button marked:'iconShowPassword'"
+
+  end
+
+
+  def forgotPasswordConfirmationAlert
+
+    "UIAlertControllerActionView"
+
+  end
+
+
+  def loginButton
+
+    "* marked:'LOG IN'"
+
+  end
+
+
+  def confirmButtonOnForgotPasswordConfirmationAlert
+
+
+    "* marked:'Reset'"
+
+  end
+
+
+  def declineButtonOnForgotPasswordConfirmationAlert
+
+
+    "* marked:'Cancel'"
+
+  end
+
+  def greetingsMessage
+
+    "label {text BEGINSWITH 'Welcome'}"
+
+  end
+
+  def userMail
+
+  	"label {text CONTAINS 'mail'}"
+  end
+
+  def userAvatar
+
+    "imageView"
+  end
+
+  def wrongPasswordBar
+
+    "view marked:'Incorrect password, Please try again!'"
+  end
+
+
+  def unableToConnectBar
+
+    "view marked:'Unable to connect'"
+
+  end
+
+  def okButton
+
+  	"view marked:'OK'"
+  end
+
 
 
 end
