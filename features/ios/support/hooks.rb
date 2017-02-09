a238e7fe6bbd7e12dc842df840d237785525f964
Denis Mordvinov <denismordvinov@Denis-Mordvinov-Mac-Mini.local>
2017-02-09 19:41:55 +0600

    01 launch has changes

--- features/ios/support/hooks.rb
+++ features/ios/support/hooks.rb
@@ -1,14 +1,12 @@
-require 'calabash-android'
-require 'calabash-cucumber'
 
-=begin
 Before('@shouldLogin') do
   
+  	sleep(5)
 	touch("button marked:'ACCESS WITH EMAIL'")
-	keyboard_enter_text("sublio1260@gmail.com")
+	keyboard_enter_text("sublio1260@gmail.com") #login
 	touch("* marked:'NEXT'") 
 	sleep(4)
-	keyboard_enter_text("avatar1260")
+	keyboard_enter_text('avatar1260') #password
 	touch("* marked:'LOG IN'")
 	sleep(6)
 
@@ -33,4 +31,7 @@ end
 
 
 
-=end
+
+
+
+
