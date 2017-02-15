6461b132995cf485afd92ca4c468b354176d8bbd
Denis Mordvinov <denismordvinov@Denis-Mordvinov-Mac-Mini.local>
2017-02-15 09:11:43 +0600

    Investor level steps fix

--- features/step_definitions/investor_type_steps.rb
+++ features/step_definitions/investor_type_steps.rb
@@ -1,6 +1,8 @@
 When(/^I select investor type$/) do
+	
   @current_page = page(InvestorTypePage).await(timeout: 30)
   @current_page.pick_first_cell(@current_page.cell)
+
   while @current_page.trait do
   	@current_page.select_investor_type(@current_page.cell, @current_page.cellText)
   	sleep(1)
