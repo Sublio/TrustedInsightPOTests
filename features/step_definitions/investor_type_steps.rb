0a445a79b6e270579cfcd27a749aebb4d5ffb98b
Denis Mordvinov <denismordvinov@Denis-Mordvinov-Mac-Mini.local>
2017-02-15 09:08:43 +0600

    Investor level test async waiting

--- features/step_definitions/investor_type_steps.rb
+++ features/step_definitions/investor_type_steps.rb
@@ -1,9 +1,9 @@
 When(/^I select investor type$/) do
   @current_page = page(InvestorTypePage).await(timeout: 30)
   @current_page.pick_first_cell(@current_page.cell)
-  sleep 3
   while @current_page.trait do
   	@current_page.select_investor_type(@current_page.cell, @current_page.cellText)
+  	sleep(1)
   end
 end
 
